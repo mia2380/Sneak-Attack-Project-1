4d7d42d559ac693ac3f0d851019cd82d6247b901
erinsawyer504 <erinsawyer504@gmail.com>
2023-01-14 10:16:53 -0500

    Updated CSS files and index, added more styling and changed colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,8 @@ var romanceMoviesEl = document.querySelector('#romance-movies');
 var actionMoviesEl = document.querySelector('#action-movies');
 var movieUl = document.querySelector('#ul-movie');
 var streamUl = document.querySelector('#ul-stream');
+var streamResultsEl = document.querySelector('#stream-container')
 
-<<<<<<< HEAD
 var bookMovies = ['Dune', 'Harry Potter'];
 
 var buttonClickHandler = function (event) {
@@ -62,6 +62,9 @@ let getStream = function (imdbID) {
 					document.getElementById("sprice").innerHTML = "Price: $" + streamPrice;
 					document.getElementById("stype").innerHTML = "Rent or Buy: " + streamType;
 					document.getElementById("surl").innerHTML = "Streaming Website: " + streamUrl;
+					document.getElementById("surl").innerHTML=`Streaming Website:  <a href=${streamUrl}>${streamUrl}</a>`;
+					//Will uncomment out once css is updated
+					streamResultsEl.className="show-results";
 
 				});
 			} else {
@@ -164,66 +167,3 @@ window.addEventListener('click', (event) => {
 		searchList.classList.add('hide-search-list');
 	}
 });
-=======
-var bookMovies = ['Dune','Harry Potter'];
-
-var buttonClickHandler = function(event) {
-    var clickedButton = event.target.getAttribute('id');
-    console.log(clickedButton);
-
-    if (clickedButton = "book-movie") {
-        var randomBookMovie = bookMovies[Math.floor(Math.random() * bookMovies.length)];  
-        console.log(randomBookMovie);
-        getMovie(randomBookMovie);
-    }
-
-}
-
-let getMovie = function(movie) {
-var queryURL = "http://www.omdbapi.com/?apikey=a454390f&page=2&type=movie&t=" + movie;
-
-    fetch(queryURL)
-    .then(function(response) {
-        if(response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                var imdbID = data.imdbID;
-                console.log(imdbID);
-                getStream(imdbID);
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-    });
-};
-
-let getStream = function(imdbID) {
-    console.log(imdbID);
-	let streamURL = "https://api.watchmode.com/v1/title/" + imdbID + "/sources/?apiKey=" + watchModeAPIKey;
-
-    fetch(streamURL)
-    .then(function(response) {
-        if(response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                var streamName = data[0].name;
-                var streamPrice = data[0].price;
-                var streamType = data[0].type;
-                var streamUrl = data[0].web_url;
-                document.getElementById("sname").innerHTML="Streaming Service: "+ streamName;
-                document.getElementById("sprice").innerHTML="Price: $"+ streamPrice;
-                document.getElementById("stype").innerHTML="Rent or Buy: "+ streamType;
-                document.getElementById("surl").innerHTML="Streaming Website: "+ streamUrl;
-
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-    });
-};
-
-//event listeners
-buttonsEl.addEventListener("click", buttonClickHandler);
->>>>>>> refs/remotes/origin/main
